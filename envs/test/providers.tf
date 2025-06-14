30b75c7cc94e6b0319944bd5d7d65f0d8336b790
sribalajirajarathinam <sribalaji7708@gmail.com>
2025-06-14 19:51:38 +0000

    modify providers.tf variables.tf pipeline.yml

--- envs/test/providers.tf
+++ envs/test/providers.tf
@@ -16,6 +16,6 @@ provider "azurerm" {
     }
 
   }
-  subscription_id = var.subscription_id
+  #subscription_id = var.subscription_id
 }
 
--- envs/test/variables.tf
+++ envs/test/variables.tf
@@ -1,9 +1,3 @@
-variable "subscription_id" {
-  description = "The Azure subscription ID where the resources will be created."
-  type        = string
-
-}
-
 variable "rg_name" {
   description = "The name of the resource group."
   type        = string
