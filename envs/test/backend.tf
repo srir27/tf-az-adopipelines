a89f77786e3ea16edd76711deec3939681d08790
sribalajirajarathinam <sribalaji7708@gmail.com>
2025-06-15 08:48:42 +0000

    modify backend.tf

--- envs/test/backend.tf
+++ envs/test/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "test-tfremote"
     storage_account_name = "tfteststorage000"
     container_name       = "tftestcontainer000"
-    key                  = "testterraform.tfstate"
+    key                  = "terraform.tfstate"
     use_azuread_auth = false
     use_msi = true
   }
